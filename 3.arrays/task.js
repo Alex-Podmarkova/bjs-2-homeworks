d9f8c52ac2ac5a09bd2f4bfb9c5ea1e67cea586a
Alex-Podmarkova <aleksandraiztredevyatogo@gmail.com>
2023-07-12 00:38:41 +0300

    Второе задание доработано

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let equallyArrays = arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
-  return equallyArrays;
+  return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-	let result = users.filter(user.gender === gender).map(user => userAge).reduce((acc, userAge, index, user) => {
-		acc += userAge;
-		if(index === user.length - 1) {
-			return acc / user.length;
-		}
-		return acc;
-	} )
   
+  return users.filter(user => user.gender == gender).map(user => user.age).reduce((acc, age, index, array) => acc + age/array.length, 0);
+    
 }
